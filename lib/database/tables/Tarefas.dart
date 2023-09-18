82732731db9058f0d061e785b8d89f9c17ce61af
DiogoVitorino-dev <diogovf90@hotmail.com>
2023-09-18 13:49:49 -0300

    final commit

--- lib/database/tables/Tarefas.dart
+++ lib/database/tables/Tarefas.dart
@@ -2,8 +2,8 @@ String create_tarefas() {
   return
      '''CREATE tarefas {
 		id INT PRIMARY KEY AUTOINCREMENT,
-		descricao:TEXT,
-		concluido:INTERGER
+		descricao TEXT,
+		concluido INTERGER
 	};''';
 }
 
--- lib/shared/widgets/appDrawer.dart
+++ lib/shared/widgets/appDrawer.dart
@@ -138,7 +138,6 @@ class AppDrawer extends StatelessWidget {
                             ),
                           ],
                         ));
-                    ;
                   },
                 );
               },
